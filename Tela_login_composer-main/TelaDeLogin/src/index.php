8adfb214c82626512b88c56e1770b6fa5ea94c63
Lucas Lemos <124939307+LukassLemos@users.noreply.github.com>
2023-06-22 19:53:17 -0300

    Initial commit

---
+++ Tela_login_composer-main/TelaDeLogin/src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+            <form method="post" action="main.php">   
+                <Label for="name">Nome Usuario:</label>
+                <input type="text" name="nome" required><br>
+
+                <Label for="pass">Senha:</label>
+                <input type="password" name="pass"></textarea><br>
+
+                
+                <input type="submit" value="Enviar" name="bntSubmitInsertTaskForm">
+            </form>
+    </body>
+</html>
